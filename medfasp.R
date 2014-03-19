82d564c33200642f908bf62902ff336de09636e7
jgmeyerucsd <jgmeyer@ucsd.edu>
2014-03-18 18:44:59 -0700

    Update medfasp.R

--- medfasp.R
+++ medfasp.R
@@ -1,10 +1,23 @@
 #### use this function to simulate MED-FASP cleavage with iterative digestion
 #### As used in the manuscript - 10,000 MWCO filtration during digestion with final filtration based
 #### on peptide length
+####  set integer seed for random number generation for use with the missed cleavage propensity
+#set.seed(420, kind = NULL, normal.kind = NULL)
+#runif(1)
 
-medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=10000,filter=length){
+medfasp=function(object=cere,proteases=c("lysC"),filterMW=TRUE,filterLEN=FALSE,LENcutoff=c(8,35),MWcutoff=5000, missed.propensity=c()){
 	###	iteratively digest with n proteases, removing those below cutoff
+	lowCO<-LENcutoff[1]
+	highCO<-LENcutoff[2]
 
+	### make sure that if missed.propensity!=0 the length matches # of proteases
+	if(length(missed.propensity!=0)){
+		if(length(proteases)==length(missed.propensity))print("using missed cleavages")
+		if(length(proteases)!=length(missed.propensity)){
+			print("# of proteases does not match length of missed.propensity")
+			return()
+			}
+		}
 	### first part call cut.predict ###
 	if(proteases[1]=="trpC"){
 		tempproteome<-cut.predict(object,residues=c("W"),term=c(1),protease="trpC")
@@ -25,29 +38,32 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 		tempproteome<-cut.predict(object,residues<-c("R"),term<-1)
 		}
 	if(proteases[1]=="lysC"){
-		tempproteome<-cut.predict(object,residues<-c("K"),term<-1,)
+		tempproteome<-cut.predict(object,residues<-c("K"),term<-1)
 		}
 	if(proteases[1]=="NTCB"){
-		tempproteome<-cut.predict(object,residues<-c("C"),term<-0,)
+		tempproteome<-cut.predict(object,residues<-c("C"),term<-0)
 		}
 	### decide which peptides are smaller than cutoff
 	temppeps<-unlist(tempproteome@cleaved)
 	peptides.len<-length(temppeps)
 	peplengths<-nchar(temppeps)
-	masses<-sapply(FUN=calculatePepM,temppeps)
-	names(masses)<-temppeps
-	if(filter==MWCO){
-		filtered<-filterMWCO(masses,cutoff)
+	names(temppeps)<-temppeps
+	if(filterMW==TRUE){
+		filtered<-MWCOfilter(temppeps,MWcutoff)
+		flowthrough<-filtered[[1]]
+		overcutoff<-filtered[[2]]
 		}
-	
+	if(filterLEN==TRUE){
+		flowthrough<-names(temppeps[peplengths<cutoff[2]])
+		overcutoff<-names(temppeps[peplengths>cutoff[2]])
+		}
 	proteases.l<-length(proteases)
 	if(proteases.l>=2){
 		### loop over proteases from 2:length(proteases)
 		for(q in 2:proteases.l){
-			print(q)
+			#print(q)
 			proteins.l<-length(overcutoff)
-			print(proteins.l)
-		
+			#print(proteins.l)
 			### set residues and termini based on protease
 			if(proteases[q]=="CNBr"){
 				residues<-c("M")
@@ -57,7 +73,6 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 				residues<-c("R","K")
 				term<-1
 			}
-			
 			if(proteases[q]=="aspN"){
 				residues<-c("D")
 				term<-0
@@ -82,9 +97,7 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 				residues<-c("C")
 				term<-0
 			}
-
-
-			## now take the peptides with mass over cutoff and cleave then similar to cut.predict
+			## now take the peptides with mass over cutoff and cleave then similar to cut.predict()
 			peps<-c(rep(0,sum(nchar(unlist(overcutoff)))))
 			peps2<-c(rep(0,sum(nchar(unlist(overcutoff)))))
 			m<-1
@@ -96,7 +109,7 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 						k<-1
 						if(term[1]==0){
 							for(j in 2:pro.l){
-								if(pro[j]==residues[1]){
+								if(pro[j]==residues[1]  & runif(1)>=missed.propensity){
 									end<-j-1
 									peps[m]<-c(paste(pro[k:end],collapse=""))
 									k<-j
@@ -112,7 +125,7 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 					if(term[1]==1){
 						if(residueslen==1){
 							for(j in 1:pro.l){
-								if(pro[j]==residues){
+								if(pro[j]==residues  & runif(1)>=missed.propensity){
 									peps[m]<-c(paste(pro[k:j],collapse=""))
 									k<-j+1
 									m<-m+1
@@ -125,7 +138,7 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 							}
 						if(residueslen==2){
 							for(j in 1:pro.l){
-								if(pro[j]==residues[1]|pro[j]==residues[2]){
+								if(pro[j]==residues[1]  & runif(1)>=missed.propensity |pro[j]==residues[2] & runif(1)>=missed.propensity){
 									peps[m]<-c(paste(pro[k:j],collapse=""))
 									k<-j+1
 									m<-m+1
@@ -137,25 +150,29 @@ medfasp=function(object=Scerevis,proteases=c("CNBr","LysC","trypsin"),cutoff=100
 								}
 							}
 						}
-					print(i/proteins.l)
+					# for debugging
+					#print(i/proteins.l)
 					}
-				
-				
-			#print(residues[1:n])
-		
-			######	set overcutoff to those still too large 
 			peps<-peps[peps!=0]
 			peptides.len<-length(peps)
 			peplengths<-nchar(peps)
-			masses<-sapply(FUN=calculatePepM,peps)
-			flowthrough<-c(flowthrough,names(masses[masses<cutoff]))
-			overcutoff<-names(masses[masses>=cutoff])
+			names(peps)<-peps
+			#### 	filter new peptide populations
+			if(filterLEN==TRUE){
+				flowthrough<-names(peps[peplengths<cutoff[2]])
+				overcutoff<-names(peps[peplengths>cutoff[2]])
+				}
+			if(filterMW==TRUE){
+				filtered<-MWCOfilter(temppeps=peps,MWcutoff)
+				flowthrough<-c(flowthrough,filtered[[1]])
+				overcutoff<-filtered[[2]]
+				}
 			}
 		}
-
-	###### finally calculate the percent coverage
-	notcovered<-sum(nchar(overcutoff))+sum(nchar(flowthrough[nchar(flowthrough)<=6])+sum(nchar(flowthrough[nchar(flowthrough)>=36]))
-	covered<-sum(nchar(flowthrough[nchar(flowthrough)>7])+nchar(flowthrough[nchar(flowthrough)<36]))
+	###### calculate the percent coverage based on peptides above lower LENcutoff
+	notcovered<-sum(nchar(overcutoff)) + sum(nchar(flowthrough[nchar(flowthrough)<lowCO]))
+	covered<-sum(nchar(flowthrough[nchar(flowthrough)>=lowCO]))
+	print(notcovered+covered)
 	print("ratio of amino acids within cutoff")
 	print(covered/(notcovered+covered))
 	return(flowthrough)
